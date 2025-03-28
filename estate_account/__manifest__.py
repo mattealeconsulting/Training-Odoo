d48a8bbca2f01ecd4c989ca61d4436b2ea2f295d
Sanzianu Gabriela <gabriela.sanzianu@mattealeconsulting.com>
2025-03-28 15:24:59 +0200

    Chapters 12 to 14

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Real Estate Accounting",
+    "summary": "Accounting",
+    "version": "18.0.1.0.0",
+    "license": "OEEL-1",
+    "depends": ["gabriela_estate", "account"],
+    "data": [
+        # Views
+        "views/estate_accounts_views.xml"
+    ],
+    "demo": [
+    ],
+    "application": True
+}
